c6ac0579d0bcb6d009268281641cf8e67cf5d7c9
Almuhannad <almuhannad.khalid@gmail.com>
2023-12-08 23:04:40 +0300

    first commit

---
+++ src/main/java/com/example/articlesportal/ArticlesPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.articlesportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/articlesportal/ArticlesPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.articlesportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
