58b40150c4126e42a114e23d9587037badd6a329
Almuhannad <almuhannad.khalid@gmail.com>
2023-12-10 11:44:08 +0300

    payload mod for user

--- src/main/java/com/example/articlesportal/dto/UserDto.java
+++ src/main/java/com/example/articlesportal/dto/UserDto.java
@@ -1,4 +1,34 @@
 package com.example.articlesportal.dto;
 
+import com.example.articlesportal.entity.Article;
+import com.example.articlesportal.entity.Privilege;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+import java.util.Set;
+
 public class UserDto {
+
+    @NotEmpty
+    @Size(min = 2, max = 15)
+    @Pattern(regexp = "^[a-zA-Z]+$")
+    private String username;
+
+    @NotEmpty
+    @Pattern(regexp = "^\\d{10}$")
+    private String mobileNumber;
+
+    @NotEmpty(message = "password must not be empty")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{5,}$")
+    private String password;
+
+    @NotEmpty
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+    private String email;
+
+    private List<Article> articles;
+
+    private Set<Privilege> privileges;
 }
